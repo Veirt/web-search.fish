7e6c24d967c528df34bfef33228f0fd3ee1e45bc
Muhammad Dony Mulya <vveirt@gmail.com>
2022-01-02 23:01:35 +0800

    feat: add web-search function

---
+++ functions/web-search.fish
@@ -0,0 +1,61 @@
+function web-search -d "Search on web"
+    switch $argv[1]
+        case google
+            set url "https://www.google.com/search?q="
+
+        case bing
+            set url "https://www.bing.com/search?q="
+
+        case yahoo
+            set url "https://search.yahoo.com/search?p="
+
+        case duckduckgo
+            set url "https://www.duckduckgo.com/?q="
+
+        case startpage
+            set url "https://www.startpage.com/do/search?q="
+
+        case yandex
+            set url "https://yandex.ru/yandsearch?text="
+
+        case github
+            set url "https://github.com/search?q="
+
+        case baidu
+            set url "https://www.baidu.com/s?wd="
+
+        case ecosia
+            set url "https://www.ecosia.org/search?q="
+
+        case goodreads
+            set url "https://www.goodreads.com/search?q="
+
+        case qwant
+            set url "https://www.qwant.com/?q="
+
+        case givero
+            set url "https://www.givero.com/search?q="
+
+        case stackoverflow
+            set url "https://stackoverflow.com/search?q="
+
+        case wolframalpha
+            set url "https://www.wolframalpha.com/input/?i="
+
+        case archive
+            set url "https://web.archive.org/web/*/"
+
+        case scholar
+            set url "https://scholar.google.com/scholar?q="
+
+        ### DEFAULT
+        case '*'
+            echo "'$argv[1]' is not supported."
+            return 1
+    end
+
+    if test (count $argv) -gt 1
+        set query (string escape --style=url $argv[2..])
+        xdg-open "$url$query"
+    end
+end
