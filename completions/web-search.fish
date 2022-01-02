f765023ebfdd4334773a8d4f55457d024c106d14
Muhammad Dony Mulya <vveirt@gmail.com>
2022-01-03 00:01:59 +0800

    feat: add completions

---
+++ completions/web-search.fish
@@ -0,0 +1,5 @@
+set -l contexts google bing yahoo duckduckgo startpage yandex github baidu ecosia goodreads qwant givero stackoverflow wolframalpha archive scholar
+
+for context in $contexts
+    complete -c web-search -f -n __fish_use_subcommand -a $context
+end
