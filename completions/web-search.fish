7991486bc6d43872c1dd8656f8453f783bfb2c91
Veirt <vveirt@gmail.com>
2023-04-19 00:23:28 +0800

    feat: add custom search entries to completion

--- completions/web-search.fish
+++ completions/web-search.fish
@@ -1,5 +1,10 @@
 set -l contexts google youtube bing yahoo duckduckgo startpage yandex github baidu ecosia goodreads qwant givero stackoverflow wolframalpha archive scholar nixpkgs nixoptions aur gpo searx brave
 
+for var in (env | grep ^WEB_SEARCH)
+    set -l context (string split -m 1 = $var)[1]
+    set -a contexts ( echo "$context" | string sub -s 12)
+end
+
 for context in $contexts
     complete -c web-search -f -n __fish_use_subcommand -a $context
 end
