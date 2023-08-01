dd99f4f91d71f12addb986d6203f7cd0f5740ec6
Veirt <55097092+Veirt@users.noreply.github.com>
2023-08-01 23:15:31 +0800

    refactor: use cleaner approach

--- completions/web-search.fish
+++ completions/web-search.fish
@@ -1,10 +1,8 @@
 set -l contexts google youtube bing yahoo duckduckgo startpage yandex github baidu ecosia goodreads qwant givero stackoverflow wolframalpha archive scholar nixpkgs nixoptions aur gpo searx brave urbandict
 
-for var in (env | grep ^WEB_SEARCH)
-    set -l context (string split -m 1 = $var)[1]
+for custom_env in (env | grep ^WEB_SEARCH)
+    set -l context (string split -m 1 = $custom_env)[1]
     set -a contexts ( echo "$context" | string sub -s 12)
 end
 
-for context in $contexts
-    complete -c web-search -f -n __fish_use_subcommand -a $context
-end
+complete -c web-search -f -n __fish_use_subcommand -a "$contexts"
--- functions/web-search.fish
+++ functions/web-search.fish
@@ -1,98 +1,47 @@
-function web-search -d "Search on web"
-    switch $argv[1]
-        case google
-            set url "https://www.google.com/search?q="
+function web-search -d "Search on the web"
+    set -l search_engines \
+    google 'https://www.google.com/search?q=' \
+    youtube "https://www.youtube.com/results?search_query=" \
+    bing "https://www.bing.com/search?q=" \
+    yahoo "https://search.yahoo.com/search?p=" \
+    duckduckgo "https://www.duckduckgo.com/?q=" \
+    startpage "https://www.startpage.com/do/search?q=" \
+    yandex "https://yandex.ru/yandsearch?text=" \
+    github "https://github.com/search?q=" \
+    baidu "https://www.baidu.com/s?wd=" \
+    ecosia "https://www.ecosia.org/search?q=" \
+    goodreads "https://www.goodreads.com/search?q=" \
+    qwant "https://www.qwant.com/?q=" \
+    givero "https://www.givero.com/search?q=" \
+    stackoverflow "https://stackoverflow.com/search?q=" \
+    wolframalpha "https://www.wolframalpha.com/input/?i=" \
+    archive "https://web.archive.org/web/*/" \
+    scholar "https://scholar.google.com/scholar?q=" \
+    nixpkgs "https://search.nixos.org/packages?query=" \
+    nixoptions "https://search.nixos.org/options?query=" \
+    aur "https://aur.archlinux.org/packages?K=" \
+    gpo "https://gpo.zugaina.org/Search?search=" \
+    searx "https://searx.org/search?q=" \
+    brave "https://search.brave.com/search?q=" \
+    urbandict "https://www.urbandictionary.com/define.php?term=" 
 
-        case youtube
-            set url "https://www.youtube.com/results?search_query="
+    set -l custom_env_engines (env | grep ^WEB_SEARCH)
+    set -l custom_search_engines (string split '\n' $custom_env_engines)
+    for engine in $custom_search_engines
+        set -l search_engine (string split -m 1 = $engine)
+        set -l context (echo $search_engine[1] | string sub -s 12)
+        set -l url $search_engine[2]
 
-        case bing
-            set url "https://www.bing.com/search?q="
-
-        case yahoo
-            set url "https://search.yahoo.com/search?p="
-
-        case duckduckgo
-            set url "https://www.duckduckgo.com/?q="
-
-        case startpage
-            set url "https://www.startpage.com/do/search?q="
-
-        case yandex
-            set url "https://yandex.ru/yandsearch?text="
-
-        case github
-            set url "https://github.com/search?q="
-
-        case baidu
-            set url "https://www.baidu.com/s?wd="
-
-        case ecosia
-            set url "https://www.ecosia.org/search?q="
-
-        case goodreads
-            set url "https://www.goodreads.com/search?q="
-
-        case qwant
-            set url "https://www.qwant.com/?q="
-
-        case givero
-            set url "https://www.givero.com/search?q="
-
-        case stackoverflow
-            set url "https://stackoverflow.com/search?q="
-
-        case wolframalpha
-            set url "https://www.wolframalpha.com/input/?i="
-
-        case archive
-            set url "https://web.archive.org/web/*/"
-
-        case scholar
-            set url "https://scholar.google.com/scholar?q="
-
-        case nixpkgs
-            set url "https://search.nixos.org/packages?query="
-
-        case nixoptions
-            set url "https://search.nixos.org/options?query="
-
-        case aur
-            set url "https://aur.archlinux.org/packages?K="
-
-        case gpo
-            set url "https://gpo.zugaina.org/Search?search="
-
-        case searx
-            set url "https://searx.org/search?q="
-
-        case brave
-            set url "https://search.brave.com/search?q="
-
-        case urbandict
-            set url "https://www.urbandictionary.com/define.php?term="
-
-        ### DEFAULT
-        case '*'
-            set custom_search
-            set custom_urls
-            for var in (env | grep ^WEB_SEARCH)
-                set -l context (string split -m 1 = $var)[1]
-                set -a custom_search ( echo "$context" | string sub -s 12)
-
-                set -l url (string split -m 1 = $var)[2]
-                set -a custom_urls $url
-            end
-
-            if contains $argv[1] $custom_search
-                set -l index (contains -i $argv[1] $custom_search)
-                set url $custom_urls[$index]
-            else
-                echo "'$argv[1]' is not supported."
-                return 1
-            end               
+        set -a search_engines $context $url
     end
 
+    set -l context_idx (contains -i $argv[1] $search_engines)
+    if not test -n "$context_idx"
+        echo "'$argv[1]' is not supported."
+        return 1
+    end
+
+    set -l url $search_engines[(math $context_idx + 1)] # + 1 in this case to get the url
 
     if test (count $argv) -ge 2
         set query (string escape --style=url $argv[2..-1])
